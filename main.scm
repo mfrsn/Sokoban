67e8efd16fb26d8310190b5591634e173186f19b
Marcus Eriksson <marcus.emejl@gmail.com>
2011-03-23 21:56:09 +0100

    Added main.scm

---
+++ main.scm
@@ -0,0 +1,8 @@
+;=================================================
+; PRAM 2011, Senast ändrad 2011-03-23
+; Projekt: Sokoban
+; Mattias Fransson, Marcus Eriksson, grupp 4, Y1a
+;
+; Fil: main.scm
+; Beskrivning: Define shit here
+;=================================================
