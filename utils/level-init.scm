8cf375e9e8f16bc27b34a0dcf9e76811c9a0d6dd
Mattias Fransson <mattias.fransson2@gmail.com>
2011-03-31 13:36:38 +0200

    ...

--- utils/level-init.scm
+++ utils/level-init.scm
@@ -16,14 +16,14 @@
 ; Parsar en data-struktur (lista med listor) och returnerar
 ; ett board-objekt.
 ; EJ KLAR!!!!!
-(define (parse-level-data data)
+(define (parse-level-data data *player*)
   (if (or (null? data) (not(list? data)))
       (error "Invalid level data. Given:" data)
       (let* ((map-height (length data))
              (map-width (length (car data)))
              (level (new board%
-                         [size-x map-width]
-                         [size-y map-height])))
+                         [size-x (+ map-width 0)]
+                         [size-y (+ map-height 0)])))
         
         ; Iterera genom raderna
         (define (iter-rows row row-list)
@@ -31,13 +31,43 @@
           ; Iterera genom kolonnerna
           (define (iter-cols col col-list)
             (cond ((null? col-list) (newline))
-                  (else (display (car col-list))
-                        (iter-cols (+ col 1) (cdr col-list)))))
+                  ((string=? (car col-list) "w")
+                   (add-floor level (make-position col row) (create-floor 'wall 'empty))
+                   (iter-cols (+ col 1) (cdr col-list)))
+                  ((string=? (car col-list) "f")
+                   (add-floor level (make-position col row) (create-floor 'floor 'empty))
+                   (iter-cols (+ col 1) (cdr col-list)))
+                  ((string=? (car col-list) "g")
+                   (add-floor level (make-position col row) (create-floor 'goal 'empty))
+                   (iter-cols (+ col 1) (cdr col-list)))
+                  ((string=? (car col-list) "v")
+                   (add-floor level (make-position col row) (create-floor 'void 'empty))
+                   (iter-cols (+ col 1) (cdr col-list)))
+                  ((string=? (car col-list) "b")
+                   (add-floor level (make-position col row) (create-block (make-position col row)))
+                   (iter-cols (+ col 1) (cdr col-list)))
+                  ((string=? (car col-list) "x")
+                   (add-floor level (make-position col row) (create-floor 'floor *player*)))
+                   (iter-cols (+ col 1) (cdr col-list)))
+                  )
           
           (cond ((null? row-list) (void))
                 (else (iter-cols 0 (car row-list))
                       (iter-rows (+ row 1) (cdr row-list)))))
         
         ; Kör!
-        (iter-rows 0 data))))
-    
+        (iter-rows 0 data)
+        level)))
+
+(define (create-floor type object)
+  (new floor%
+       [type type]
+       [current-object object]))
+
+(define (add-floor board position floor-object)
+  (send board set-square! position floor-object))
+
+(define (create-block position)
+  (let ((block (new block%
+                    [current-position position])))
+    (create-floor 'floor block)))
