ce86018a724668e07acd8d41e05c91e0bad582c0
Mattias Fransson <mattias.fransson2@gmail.com>
2011-05-10 16:38:45 +0200

    blahbla

--- gameloop.scm
+++ gameloop.scm
@@ -36,8 +36,8 @@
 (define *main-menu-active?* #t)
 
 ; Anslutning till servern
-(define *port* 23407)
-(define *host* "localhost")
+(define *port* 23408)
+(define *host* "130.236.71.123")
 
 ; Ladda in nivåfilerna
 (define *number-of-maps* 6)
